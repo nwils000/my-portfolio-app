62836ed43e144e3a82b145bbb63da7915d3f9c72
nwils000 <nwils000@gmail.com>
2023-01-13 12:59:45 -0500

    made navbar responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,10 @@ export default function Home() {
       <div className="name__wrapper">
         <Navbar />
         <div href="#" className="hover-this">
-          <div className="home__name">Nathan Wilson</div>
+          <div className="home__name">
+            <h1>Nathan Wilson</h1>
+            <h2>Web Developer</h2>
+          </div>
         </div>
         <div className="cursor"></div>
       </div>
