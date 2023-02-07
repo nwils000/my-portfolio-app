e6b9703757ede1fe30b530b93794d0846f28cbcc
nwils000 <100631201+nwils000@users.noreply.github.com>
2023-02-07 17:10:51 -0500

    Edited the text in the about component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,12 +13,12 @@ export default function About({ handleHover, handleLeave, icon, cursorClass }) {
       <div className="about__container">
         <div className="about__content">
           <h1>Hello,</h1>
-          <h2>I am a creator with a strong passion for web development</h2>
+          <h2>I am a creator with a strong passion for web development.</h2>
           <h3>
-            Over the last year and half I have become obsessed with creating
-            beautiful, unique, and functional websites for a variety of
-            different purposes. My natural creativity and close attention to
-            detail have helped me excell quickly in this field.
+            Over the last year and a half, I have become obsessed with creating
+            beautiful, unique, and functional websites. My natural creativity
+            and close attention to detail have helped me excel quickly in this
+            field.
           </h3>
           <h2>Skills</h2>
           <ul>
@@ -43,27 +43,28 @@ export default function About({ handleHover, handleLeave, icon, cursorClass }) {
           <div className="reason__block">
             <h4>1:</h4>
             <h3>
-              I have all the skills necesary to succeed in the job I'm applying
-              for. If theres any additional skills / speficic tools I need to
-              learn to work for you, I guarantee that I will learn those and
-              become efficient in them quickly
+              I have all the skills necessary to succeed in the job you're
+              considering me for. If there are any additional skills / specific
+              tools I need to learn to work for you, I guarantee that I will
+              learn and become efficient with them quickly.
             </h3>
           </div>
           <div className="reason__block">
             <h4>2:</h4>
             <h3>
-              I'm motivated. I'm motivated by my passion in web development. I'm
-              motivated by my wife and daughter to support them financially. I'm
-              motivated by my natural desire to excell in all that I do.
+              I'm motivated. I'm motivated by my passion for web development.
+              I'm motivated by my wife and daughter to support them financially.
+              I'm motivated by my natural desire to excel in all that I do.
             </h3>
           </div>
           <div className="reason__block">
             <h4>3:</h4>
             <h3>
-              I've been a great leader and I've also been a great follower. I
+              I've been a great leader, and I've also been a great follower. I
               have good interpersonal skills. I know how to take and give
-              compliment as well as constructive criticism. I dont enjoy drama,
-              you'll never have a problem with my conduct in the work space.
+              compliments as well as constructive criticism. I don't enjoy
+              drama, and you'll never have a problem with my conduct in the
+              workplace.
             </h3>
           </div>
         </div>
