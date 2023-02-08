842b9d75543f96673e7678302b8b354f1d5d8a03
nwils000 <100631201+nwils000@users.noreply.github.com>
2023-02-08 13:23:19 -0500

    Finishes basic JSX and styling for drop down menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import circle from "../Photos/circle.png";
 
 export default function Navbar({ cursorClass }) {
+  const [dropdownClass, setDropdownClass] = useState("dropdown");
+
   return (
     <div className="nav__container">
+      <div className={dropdownClass}>
+        <button
+          onClick={() => setDropdownClass("dropdown__active")}
+          className="circle"
+        >
+          <img src="" alt="" />
+        </button>
+        <div className="cross">
+          <button onClick={() => setDropdownClass("dropdown")}>
+            <img src="" alt="" />
+          </button>
+          <div className="dropdown-links__flex">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </div>
+      </div>
       <div className="nav__line line1">
         <h2 className="heading1">Home</h2>
         <Link className="nav__link" to="/">
