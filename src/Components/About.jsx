4ac3de9b79b9593740bfa74da1142274d1c70e5e
nwils000 <100631201+nwils000@users.noreply.github.com>
2023-02-09 17:58:47 -0500

    Changed the wording of the about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -39,12 +39,11 @@ export default function About({ cursorClass }) {
         </div>
         <div className="about__content">
           <h1>Hello,</h1>
-          <h2>I'm a creator with a strong passion for web development.</h2>
+          <h2>I'm a Front-end Web Developer.</h2>
           <h3>
-            Over the last year and a half, I have become obsessed with creating
-            beautiful, unique, and functional websites. My natural creativity
-            and close attention to detail have helped me excel quickly in this
-            field.
+            I can create the front end of any website. I'm skilled in HTML, CSS,
+            JavaScript, and the frameworks/libraries listed below. I primarily
+            work with React and am willing to work with other libraries.
           </h3>
           <h2>Skills</h2>
           <ul>
@@ -55,45 +54,13 @@ export default function About({ cursorClass }) {
             <li>Redux</li>
             <li>Git</li>
             <li>Bootstrap</li>
-            <li>CSS Sprites</li>
             <li>Sass</li>
             <li>VS Code</li>
-            <li>HandBrake</li>
+            <li>CSS Sprites</li>
             <li>Communicating with API's</li>
             <li>DOM Manipulation</li>
             <li>Routing</li>
-            <li>Custom Fonts</li>
           </ul>
-
-          <h2>Why Hire Me?</h2>
-          <div className="hire-me__intro">Heres a few reasons...</div>
-          <div className="reason__block">
-            <h4>1:</h4>
-            <h3>
-              I have all the skills necessary to succeed in the job you're
-              considering me for. If there are any additional skills / specific
-              tools I need to learn to work for you, I guarantee that I will
-              learn and become efficient with them quickly.
-            </h3>
-          </div>
-          <div className="reason__block">
-            <h4>2:</h4>
-            <h3>
-              I'm motivated. I'm motivated by my passion for web development.
-              I'm motivated by my wife and daughter to support them financially.
-              I'm motivated by my natural desire to excel in all that I do.
-            </h3>
-          </div>
-          <div className="reason__block">
-            <h4>3:</h4>
-            <h3>
-              I've been a great leader, and I've also been a great follower. I
-              have good interpersonal skills. I know how to take and give
-              compliments as well as constructive criticism. I don't enjoy
-              drama, and you'll never have a problem with my conduct in the
-              workplace.
-            </h3>
-          </div>
         </div>
       </div>
     </div>
