fd5d674464dc0f7e2aff7cfca97fbe4e6523feaa
nwils000 <100631201+nwils000@users.noreply.github.com>
2023-02-09 20:45:32 -0500

    added to about text

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -39,7 +39,7 @@ export default function About({ cursorClass }) {
         </div>
         <div className="about__content">
           <h1>Hello,</h1>
-          <h2>I'm a Front-end Developer.</h2>
+          <h2>I'm a Front-end Web Developer.</h2>
           <h3>
             I'm very proficient in HTML, CSS, JavaScript, React, and the
             frameworks/libraries listed below.
